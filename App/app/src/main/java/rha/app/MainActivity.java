9989edd59948b3f42cc86cf4a4a4edcc6891a6c8
ramirodoi <ramirodoi@gmail.com>
2015-08-29 23:03:10 -0300

    +LoginActivity.

--- App/app/src/main/java/rha/app/MainActivity.java
+++ App/app/src/main/java/rha/app/MainActivity.java
@@ -1,10 +1,12 @@
 package rha.app;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 
 import com.parse.GetCallback;
 import com.parse.Parse;
@@ -14,11 +16,17 @@ import com.parse.ParseQuery;
 import java.util.List;
 
 public class MainActivity extends ActionBarActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btnCrearUsuario).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
 
         // Enable Local Datastore.
         Parse.enableLocalDatastore(this);
