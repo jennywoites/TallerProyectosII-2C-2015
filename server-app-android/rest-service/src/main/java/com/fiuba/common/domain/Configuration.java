5eb2d482807e84610a0a381d382eb7d49089ec9e
Nicolas Menzano Diaz <nicolasdiaz55@yahoo.com.ar>
2015-09-16 21:56:04 -0300

    Borro todo (?)

--- server-app-android/rest-service/src/main/java/com/fiuba/common/domain/Configuration.java
+++
@@ -1,6 +0,0 @@
-package com.fiuba.common.domain;
-
-public class Configuration {
-
-  public static int TIME_OUT_SECONDS = 30;
-}
--- server-app-android/rest-service/src/main/java/com/fiuba/common/domain/Task.java
+++
@@ -1,6 +0,0 @@
-package com.fiuba.common.domain;
-
-public enum Task {
-
-  GET_USER_BY_NAME;
-}
