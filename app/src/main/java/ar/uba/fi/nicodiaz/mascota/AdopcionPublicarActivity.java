7903c1492abfe321b05bf661fad69e8396aaf5dd
nicodiaz55 <nicolasdiaz55@yahoo.com.ar>
2015-09-18 00:14:38 -0300

    #20 Probando cargar una imagen desde la galería.

--- app/src/main/java/ar/uba/fi/nicodiaz/mascota/AdopcionPublicarActivity.java
+++ app/src/main/java/ar/uba/fi/nicodiaz/mascota/AdopcionPublicarActivity.java
@@ -1,23 +1,32 @@
 package ar.uba.fi.nicodiaz.mascota;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.parse.ParseObject;
 
-import butterknife.InjectView;
+import java.io.IOException;
 
 public class AdopcionPublicarActivity extends AppCompatActivity {
 
+    private static final int PICK_IMAGE = 1046;
     private Toolbar toolbar;
+    private ImageView image;
+    private Button selectImageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +37,34 @@ public class AdopcionPublicarActivity extends AppCompatActivity {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        image = (ImageView) findViewById(R.id.imageView);
+        selectImageButton = (Button) findViewById(R.id.button1);
+        selectImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onPickPhoto();
+            }
+        });
+    }
+
+    public void onPickPhoto() {
+        Intent pickIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        pickIntent.setType("image/*");
+        startActivityForResult(pickIntent, PICK_IMAGE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (data != null) {
+            Uri photoUri = data.getData();
+            try {
+                Bitmap selectedImage = MediaStore.Images.Media.getBitmap(this.getContentResolver(), photoUri);
+                image.setImageBitmap(selectedImage);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
