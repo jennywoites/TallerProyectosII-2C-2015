4536d6573e285723c1adae0f06405d697b1d8ffd
nicodiaz55 <nicolasdiaz55@yahoo.com.ar>
2015-09-20 19:28:29 -0300

    #27 Incorporada vista de video (de forma medio trucha) a la mascota. Después se analizará si hay que tener embebido el reproductor ahí (en ese caso, hay que registrar la app con google o no se que onda jaja)

--- app/src/main/java/ar/uba/fi/nicodiaz/mascota/MascotaDetalleActivity.java
+++ app/src/main/java/ar/uba/fi/nicodiaz/mascota/MascotaDetalleActivity.java
@@ -1,12 +1,18 @@
 package ar.uba.fi.nicodiaz.mascota;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.daimajia.slider.library.Indicators.PagerIndicator;
 import com.daimajia.slider.library.SliderLayout;
@@ -77,6 +83,27 @@ public class MascotaDetalleActivity extends AppCompatActivity {
         photo_slider.setPresetTransformer(SliderLayout.Transformer.RotateDown);
         photo_slider.setPresetIndicator(SliderLayout.PresetIndicators.Center_Bottom);
         photo_slider.setCustomIndicator((PagerIndicator) findViewById(R.id.custom_indicator));
+
+
+        // TODO: pedir de la base de datos con el ID recibido por el intent el id de video que tenga:
+        // El id del video es el que aparece en la url de youtube del mismo. Eso habria que guardar.
+
+        final String video_id = "ycdcDFuGarM";
+
+        Button video_button = (Button) findViewById(R.id.video_button);
+        video_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(),"Click",Toast.LENGTH_SHORT).show();
+                try {
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("vnd.youtube:" + video_id));
+                    startActivity(intent);
+                } catch (ActivityNotFoundException e) {
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.youtube.com/watch?v=" + video_id));
+                    startActivity(intent);
+                }
+            }
+        });
     }
 
     @Override
