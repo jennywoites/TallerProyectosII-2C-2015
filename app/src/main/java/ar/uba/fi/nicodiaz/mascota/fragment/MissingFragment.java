def35bae12c424a142816465587f61f8ab11f52d
nicodiaz55 <nicolasdiaz55@yahoo.com.ar>
2015-10-04 16:28:12 -0300

    #84 - Ahora tenemos pantalla deslizante de filtros. Por el momento tiene los mismos filtros que la pantalla de adopcion (Si se cambia en uno, se cambia en ambas!!!). Investigar si quiere los mismos o no. (No lo cierro por eso).

--- app/src/main/java/ar/uba/fi/nicodiaz/mascota/fragment/MissingFragment.java
+++ app/src/main/java/ar/uba/fi/nicodiaz/mascota/fragment/MissingFragment.java
@@ -3,14 +3,28 @@ package ar.uba.fi.nicodiaz.mascota.fragment;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckedTextView;
+import android.widget.ExpandableListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import ar.uba.fi.nicodiaz.mascota.R;
+import ar.uba.fi.nicodiaz.mascota.utils.Filter;
+import ar.uba.fi.nicodiaz.mascota.utils.SettingsListAdapter;
 
 /**
  * Created by nicolas on 14/09/15.
@@ -20,6 +34,10 @@ public class MissingFragment extends Fragment {
     private static final String TAG = "MissingFragment";
 
     private Context activity;
+    private View mainView;
+    private DrawerLayout drawerLayout;
+
+    private boolean hayMas;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -29,23 +47,24 @@ public class MissingFragment extends Fragment {
         setHasOptionsMenu(true);
 
         // View:
-        View view = inflater.inflate(R.layout.fragment_missing, container, false);
-        view.setTag(TAG);
+        mainView = inflater.inflate(R.layout.fragment_missing, container, false);
+        mainView.setTag(TAG);
 
-        return view;
-    }
+        // Sliding Menu
+        drawerLayout = (DrawerLayout) mainView.findViewById(R.id.sliding_layout);
 
-    @Override
-    public void onSaveInstanceState(Bundle savedInstanceState) {
-        // TODO: si hay que guardar algo, hacerlo aca
-        // savedInstanceState.putSerializable(algo);
-        super.onSaveInstanceState(savedInstanceState);
+        hayMas = true;
+
+        // Filter:
+        createFilterMenu(mainView);
+
+        return mainView;
     }
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        inflater.inflate(R.menu.menu_home, menu);
+        inflater.inflate(R.menu.menu_missing, menu);
         super.onCreateOptionsMenu(menu, inflater);
     }
 
@@ -54,13 +73,109 @@ public class MissingFragment extends Fragment {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.action_buscar_perdido:
+                buscarPerdido();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    private void buscarPerdido() {
+        int drawer = GravityCompat.END;
+        if (drawerLayout.isDrawerOpen(drawer)) {
+            drawerLayout.closeDrawers();
+        } else {
+            drawerLayout.openDrawer(drawer);
+        }
+    }
+
+    private void applyQuery() {
+       // new PetListLoader(mainView).execute();
+    }
+
+    // Filter:
+    private SettingsListAdapter adapter;
+    private ExpandableListView filtersList;
+    private ArrayList<Filter> filters;
+    private Map<String, List<String>> selectedFilter;
+
+    private void createFilterMenu(View view) {
+        filtersList = (ExpandableListView) view.findViewById(R.id.categories);
+        filters = Filter.getFilters();
+        adapter = new SettingsListAdapter(activity,
+                filters, filtersList);
+        filtersList.setAdapter(adapter);
+
+        selectedFilter = new HashMap<>();
+
+        filtersList.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition, int childPosition, long id) {
+                CheckedTextView checkbox = (CheckedTextView) v.findViewById(R.id.list_item_text_child);
+                checkbox.toggle();
+
+                String parentName = filters.get(groupPosition).name;
+                String childName = checkbox.getText().toString();
+
+
+                // find parent view by tag
+                View parentView = filtersList.findViewWithTag(parentName);
+                if (parentView != null) {
+                    TextView sub = (TextView) parentView.findViewById(R.id.list_item_text_subscriptions);
+
+                    if (sub != null) {
+                        Filter filter = filters.get(groupPosition);
+                        if (checkbox.isChecked()) {
+                            // add child filter to parent's selection list
+                            filter.selection.add(childName);
+                        } else {
+                            // remove child filter from parent's selection list
+                            filter.selection.remove(childName);
+                        }
+
+                        // display selection list
+                        //if (filter.selection.isEmpty() || filter.selection.size() == filter.children.size()) {
+                        if (filter.selection.isEmpty()) {
+                            sub.setText("");
+                            selectedFilter.remove(parentName);
+                        } else {
+                            sub.setText(filter.selection.toString());
+                            selectedFilter.put(parentName, filter.selection);
+                        }
+                    }
+                }
+                return true;
+            }
+        });
+
+        Button filterButton = (Button) view.findViewById(R.id.filter_button);
+        filterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.closeDrawers();
+
+                Toast.makeText(activity, "Filtro Aplicado", Toast.LENGTH_SHORT).show();
+
+                // aplicar cambios a la lista
+                hayMas = true;
+                applyQuery();
+            }
+        });
+
+        Button cancelFilterButton = (Button) view.findViewById(R.id.filter_button2);
+        cancelFilterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                filters = Filter.getFilters(); // TODO: investigar que filtros va a querer el tipo. Si son los mismos que en adopcion, listo.
+                adapter = new SettingsListAdapter(activity, filters, filtersList);
+                filtersList.setAdapter(adapter);
+                selectedFilter.clear();
+                Toast.makeText(activity, "Filtro Desactivado", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
