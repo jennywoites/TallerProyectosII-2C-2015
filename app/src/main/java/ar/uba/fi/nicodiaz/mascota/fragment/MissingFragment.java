7b7ac4315be1ae1249dab0219f8ba804c01fde82
ndiaz <ndiaz@quadiontech.com>
2015-10-06 14:33:32 -0300

    #83 - Se agrega la pantalla de mascotas perdidas, identica a la de adopcion. Seguramente despues convendría hacer un refactor, con una clase en comun para las activities ya que hacen MUCHAS cosas iguales.

--- app/src/main/java/ar/uba/fi/nicodiaz/mascota/fragment/MissingFragment.java
+++ app/src/main/java/ar/uba/fi/nicodiaz/mascota/fragment/MissingFragment.java
@@ -1,10 +1,16 @@
 package ar.uba.fi.nicodiaz.mascota.fragment;
 
 import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -14,30 +20,118 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckedTextView;
 import android.widget.ExpandableListView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.parse.ParseFile;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import ar.uba.fi.nicodiaz.mascota.AdopcionPublicarActivity;
+import ar.uba.fi.nicodiaz.mascota.MascotaDetalleActivity;
 import ar.uba.fi.nicodiaz.mascota.R;
+import ar.uba.fi.nicodiaz.mascota.model.AdoptionPet;
+import ar.uba.fi.nicodiaz.mascota.model.MissingPet;
+import ar.uba.fi.nicodiaz.mascota.model.PetService;
+import ar.uba.fi.nicodiaz.mascota.utils.AdopcionEndlessAdapter;
 import ar.uba.fi.nicodiaz.mascota.utils.Filter;
+import ar.uba.fi.nicodiaz.mascota.utils.MissingEndlessAdapter;
+import ar.uba.fi.nicodiaz.mascota.utils.ParseProxyObject;
 import ar.uba.fi.nicodiaz.mascota.utils.SettingsListAdapter;
 
-/**
- * Created by nicolas on 14/09/15.
- */
 public class MissingFragment extends Fragment {
 
     private static final String TAG = "MissingFragment";
-
-    private Context activity;
-    private View mainView;
-    private DrawerLayout drawerLayout;
-
+    private List<MissingPet> list;
+    private RecyclerView listView;
+    private TextView emptyView;
     private boolean hayMas;
+    private MissingEndlessAdapter listAdapter;
+    private Context activity;
+    private DrawerLayout drawerLayout;
+    private View mainView;
+
+    private class LoadMorePets extends AsyncTask<Integer, Void, Boolean> {
+
+        List<MissingPet> resultList;
+
+        @Override
+        protected void onPreExecute() {
+            list.add(null);
+            listAdapter.notifyItemInserted(list.size() - 1);
+        }
+
+        @Override
+        protected Boolean doInBackground(Integer... currentPage) {
+            if (selectedFilter.isEmpty()) {
+                resultList = PetService.getInstance().getMissingPets(currentPage[0]);
+            } else {
+                resultList = PetService.getInstance().getMissingPets(currentPage[0], selectedFilter);
+            }
+            return !resultList.isEmpty();
+        }
+
+        @Override
+        protected void onPostExecute(Boolean result) {
+            list.remove(list.size() - 1);
+            listAdapter.notifyItemRemoved(list.size());
+            if (!result) {
+                hayMas = false;
+                return;
+            }
+
+            for (MissingPet pet : resultList) {
+                list.add(pet);
+                listAdapter.notifyItemInserted(list.size() - 1);
+            }
+            listAdapter.setLoaded();
+        }
+    }
+
+    private class PetListLoader extends AsyncTask<Void, Void, Boolean> {
+
+        private LinearLayout linlaHeaderProgress;
+        private List<MissingPet> resultList;
+
+        public PetListLoader(View view) {
+            linlaHeaderProgress = (LinearLayout) view.findViewById(R.id.linlaHeaderProgress);
+            resultList = new ArrayList<>();
+        }
+
+        @Override
+        protected void onPreExecute() {
+            list.clear();
+            listAdapter.notifyDataSetChanged();
+            linlaHeaderProgress.setVisibility(View.VISIBLE);
+            listAdapter.reset();
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            if (selectedFilter.isEmpty()) {
+                resultList = PetService.getInstance().getMissingPets(0);
+            }
+            else {
+                resultList = PetService.getInstance().getMissingPets(0, selectedFilter);
+            }
+            return !(resultList.isEmpty());
+        }
+
+        @Override
+        protected void onPostExecute(Boolean result) {
+            linlaHeaderProgress.setVisibility(View.GONE);
+            if (result) {
+                list.addAll(resultList);
+                listAdapter.notifyDataSetChanged();
+            }
+            checkEmptyList();
+            listAdapter.setLoaded();
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -50,6 +144,15 @@ public class MissingFragment extends Fragment {
         mainView = inflater.inflate(R.layout.fragment_missing, container, false);
         mainView.setTag(TAG);
 
+        // FAB
+        FloatingActionButton FAB = (FloatingActionButton) mainView.findViewById(R.id.FAB_agregar_perdido);
+        FAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nuevaPerdida();
+            }
+        });
+
         // Sliding Menu
         drawerLayout = (DrawerLayout) mainView.findViewById(R.id.sliding_layout);
 
@@ -58,12 +161,71 @@ public class MissingFragment extends Fragment {
         // Filter:
         createFilterMenu(mainView);
 
+        // ListView
+        emptyView = (TextView) mainView.findViewById(R.id.empty_view);
+
+        list = new ArrayList<>();
+
+        listView = (RecyclerView) mainView.findViewById(R.id.list_missing);
+        listView.setLayoutManager(new LinearLayoutManager(activity));
+        listView.setItemAnimator(new DefaultItemAnimator());
+        listView.setHasFixedSize(true);
+
+        listAdapter = new MissingEndlessAdapter(list, listView, activity);
+        listAdapter.setOnItemClickListener(new MissingEndlessAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(View itemView, int position) {
+                Intent i = new Intent(activity, MascotaDetalleActivity.class);
+                ArrayList<String> urlPhotos = new ArrayList<>();
+                MissingPet missingPet = list.get(position);
+                for (ParseFile picture : missingPet.getPictures()) {
+                    urlPhotos.add(picture.getUrl());
+                }
+                ArrayList<String> urlVideos = missingPet.getVideos();
+
+                ParseProxyObject ppo = new ParseProxyObject(missingPet);
+                i.putExtra("Pet", ppo);
+                i.putStringArrayListExtra("UrlPhotos", urlPhotos);
+                i.putStringArrayListExtra("UrlVideos", urlVideos);
+                startActivity(i);
+                getActivity().overridePendingTransition(R.anim.slide_in_1, R.anim.slide_out_1);
+            }
+        });
+
+        listAdapter.setOnLoadMoreListener(new MissingEndlessAdapter.OnLoadMoreListener() {
+            @Override
+            public boolean onLoadMore(int currentPage) {
+                if (hayMas) {
+                    new LoadMorePets().execute(currentPage);
+                    return true;
+                }
+                return false;
+            }
+        });
+        listView.setAdapter(listAdapter);
+
+        // Cargando la lista de mascotas:
+        new PetListLoader(mainView).execute();
+
         return mainView;
+    }
+
+    private void applyQuery() {
+        new PetListLoader(mainView).execute();
+    }
+
+    private void checkEmptyList() {
+        if (list.isEmpty()) {
+            listView.setVisibility(View.GONE);
+            emptyView.setVisibility(View.VISIBLE);
+        } else {
+            listView.setVisibility(View.VISIBLE);
+            emptyView.setVisibility(View.GONE);
+        }
     }
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         inflater.inflate(R.menu.menu_missing, menu);
         super.onCreateOptionsMenu(menu, inflater);
     }
@@ -91,8 +253,9 @@ public class MissingFragment extends Fragment {
         }
     }
 
-    private void applyQuery() {
-       // new PetListLoader(mainView).execute();
+    private void nuevaPerdida() {
+    /*    Intent i = new Intent(activity, MissingPublicarActivity.class);
+        startActivity(i);*/
     }
 
     // Filter:
---
+++ app/src/main/java/ar/uba/fi/nicodiaz/mascota/utils/MissingEndlessAdapter.java
@@ -0,0 +1,196 @@
+package ar.uba.fi.nicodiaz.mascota.utils;
+
+import android.content.Context;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.parse.GetDataCallback;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseImageView;
+import com.pnikosis.materialishprogress.ProgressWheel;
+
+import java.util.List;
+
+import ar.uba.fi.nicodiaz.mascota.R;
+import ar.uba.fi.nicodiaz.mascota.model.AdoptionPet;
+import ar.uba.fi.nicodiaz.mascota.model.MissingPet;
+import ar.uba.fi.nicodiaz.mascota.model.PetService;
+
+/**
+ * Created by nicolas on 13/09/15.
+ * <p/>
+ * From: http://stackoverflow.com/questions/30681905/adding-items-to-endless-scroll-recyclerview-with-progressbar-at-bottom/30691092#30691092
+ */
+public class MissingEndlessAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private final int VIEW_ITEM = 1;
+    private final int VIEW_PROG = 0;
+    private final RecyclerView view;
+
+    // The minimum amount of items to have below your current scroll position before loading more.
+    private int visibleThreshold = 1;
+    private int currentPage = 1; // TODO: o cero
+    private int lastVisibleItem, totalItemCount;
+    private boolean loading;
+    private OnLoadMoreListener onLoadMoreListener;
+
+    private List<MissingPet> mascotas;
+    private Context context;
+    private OnItemClickListener listener;
+
+    public interface OnItemClickListener {
+        void onItemClick(View itemView, int position);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        this.listener = listener;
+    }
+
+    public interface OnLoadMoreListener {
+        boolean onLoadMore(int currentPage);
+    }
+
+    public void setOnLoadMoreListener(OnLoadMoreListener onLoadMoreListener) {
+        this.onLoadMoreListener = onLoadMoreListener;
+    }
+
+    public void reset() {
+        loading = true;
+        totalItemCount = ((LinearLayoutManager) view.getLayoutManager()).getItemCount();
+        lastVisibleItem = ((LinearLayoutManager) view.getLayoutManager()).findLastVisibleItemPosition();
+        currentPage = 1; // TODO: o cero
+    }
+
+    public MissingEndlessAdapter(List<MissingPet> mascotas, RecyclerView recyclerView, Context context) {
+        this.mascotas = mascotas;
+        this.context = context;
+        this.view = recyclerView;
+
+        if (view.getLayoutManager() instanceof LinearLayoutManager) {
+
+            final LinearLayoutManager linearLayoutManager = (LinearLayoutManager) view.getLayoutManager();
+            recyclerView.setOnScrollListener(new RecyclerView.OnScrollListener() {
+                @Override
+                public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                    super.onScrolled(recyclerView, dx, dy);
+                    totalItemCount = linearLayoutManager.getItemCount();
+                    lastVisibleItem = linearLayoutManager.findLastVisibleItemPosition();
+                    if (!loading && totalItemCount <= (lastVisibleItem + visibleThreshold)) {
+                        // End has been reached
+                        // Do something
+                        if (onLoadMoreListener != null) {
+                            if (onLoadMoreListener.onLoadMore(currentPage)) {
+                                currentPage++;
+                            }
+                        }
+                        loading = true;
+                    }
+                }
+            });
+        }
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        RecyclerView.ViewHolder vh;
+        View v;
+        if (viewType == VIEW_ITEM) {
+            v = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_adopcion, parent, false);
+            vh = new MascotaViewHolder(v);
+        } else {
+            v = LayoutInflater.from(parent.getContext()).inflate(R.layout.progress_item, parent, false);
+            vh = new ProgressViewHolder(v);
+        }
+
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, int i) {
+        if (viewHolder instanceof MascotaViewHolder) {
+            MascotaViewHolder view = (MascotaViewHolder) viewHolder;
+
+            MissingPet mascota = mascotas.get(i);
+            ParseFile photoFile = mascota.getPicture();
+            if (photoFile != null) {
+                view.mascotaImage.setVisibility(View.VISIBLE);
+                view.mascotaImage.setParseFile(photoFile);
+                view.mascotaImage.loadInBackground(new GetDataCallback() {
+                    @Override
+                    public void done(byte[] data, ParseException e) {
+                    }
+                });
+            } else {
+                view.mascotaImage.setVisibility(View.INVISIBLE);
+            }
+
+            view.mascotaName.setText(mascota.getName());
+            view.mascotaDescription.setText(mascota.getPreviewDescription());
+
+            if (mascota.isMale()) {
+                view.mascotaDescription.setCompoundDrawablesWithIntrinsicBounds(null, null, context.getResources().getDrawable(R.drawable.ic_male), null);
+            } else {
+                view.mascotaDescription.setCompoundDrawablesWithIntrinsicBounds(null, null, context.getResources().getDrawable(R.drawable.ic_female), null);
+            }
+
+            int iconPetID = PetService.getInstance().getIconPet(mascota.getKind());
+            if (iconPetID != -1) {
+                view.mascotaName.setCompoundDrawablesWithIntrinsicBounds(null, null, context.getResources().getDrawable(iconPetID), null);
+            }
+
+            view.currentMascota = mascota;
+        } else {
+            ((ProgressViewHolder) viewHolder).progressBar.spin();
+        }
+    }
+
+    public void setLoaded() {
+        loading = false;
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return mascotas == null ? 0 : mascotas.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return mascotas.get(position) != null ? VIEW_ITEM : VIEW_PROG;
+    }
+
+    public class MascotaViewHolder extends RecyclerView.ViewHolder {
+        public TextView mascotaName;
+        public TextView mascotaDescription;
+        public ParseImageView mascotaImage;
+        public MissingPet currentMascota;
+
+        public MascotaViewHolder(final View itemView) {
+            super(itemView);
+            mascotaImage = (ParseImageView) itemView.findViewById(R.id.petImage);
+            mascotaName = (TextView) itemView.findViewById(R.id.petName);
+            mascotaDescription = (TextView) itemView.findViewById(R.id.petDescription);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (listener != null)
+                        listener.onItemClick(itemView, getLayoutPosition());
+                }
+            });
+        }
+    }
+
+    public static class ProgressViewHolder extends RecyclerView.ViewHolder {
+        public ProgressWheel progressBar;
+
+        public ProgressViewHolder(View v) {
+            super(v);
+            progressBar = (ProgressWheel) v.findViewById(R.id.progressBar);
+        }
+    }
+}
