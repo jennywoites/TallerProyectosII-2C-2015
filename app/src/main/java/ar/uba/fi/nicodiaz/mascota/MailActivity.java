632d2ecfcca71a2c6cdb62d84835013649b2687d
nicodiaz55 <nicolasdiaz55@yahoo.com.ar>
2015-10-29 17:45:43 -0300

    Fix

--- app/src/main/java/ar/uba/fi/nicodiaz/mascota/MailActivity.java
+++ app/src/main/java/ar/uba/fi/nicodiaz/mascota/MailActivity.java
@@ -1,11 +1,8 @@
 package ar.uba.fi.nicodiaz.mascota;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 import com.kristijandraca.backgroundmaillibrary.BackgroundMail;
 
@@ -14,7 +11,7 @@ public class MailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mail);
+        setContentView(R.layout.activity_home);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -25,16 +22,6 @@ public class MailActivity extends AppCompatActivity {
         bm.setFormSubject("Subject");
         bm.setFormBody("Body");
         bm.send();
-
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
